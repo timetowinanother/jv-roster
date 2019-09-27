df93849159c1c2310f6be13160082e640d9f58e3
Jonathan Martindell <jmartindell@covermymeds.com>
2019-09-27 15:44:36 -0400

    add seed for positions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+{ "Goalie" => 1,
+  "Defense" => 1,
+  "Offense" => 3,
+  "Absent" => 99}.each do |name, per_game|
+    Position.find_or_create_by(name: name, per_game: per_game)
+  end
